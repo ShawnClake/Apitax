562106339ad62820abef770c62f011f3aa4179ce
Shawn Clake <shawn.clake@gmail.com>
2018-06-07 21:04:56 -0600

    Fixed problematic build variables

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-zip -r -qq apitax-nodep-$(BUILD_TAG)-commit-$(GIT_COMMIT).zip .
+zip -r -qq apitax-nodep-$BUILD_TAG-commit-$GIT_COMMIT.zip .
 npm --prefix apitax/ah/web/node/node install
 npm --prefix apitax/ah/web/node/node run build-jenkins
-zip -r -qq apitax-dep-$(BUILD_TAG)-commit-$(GIT_COMMIT).zip .
+zip -r -qq apitax-dep-$BUILD_TAG-commit-$GIT_COMMIT.zip .
