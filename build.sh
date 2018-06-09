5de07292ea51def99164a39e47d9e486adefd2a9
Shawn Clake <shawn.clake@gmail.com>
2018-06-08 20:15:04 -0600

    Updated build again
    Signed-off-by: Shawn Clake <shawn.clake@gmail.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
+#/bin/bash
+set -euox pipefail
 zip -r -qq $BUILD_TAG-commit-$GIT_COMMIT-nodep.zip .
 /bin/bash install.sh
-python -m unittest discover -p uTest*.py
+source venv/bin/activate
+python -m unittest discover -v -p uTest*.py
 python setup.py sdist bdist_wheel
 #zip -r -qq $BUILD_TAG-commit-$GIT_COMMIT-dep.zip .
--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#/bin/bash
+set -euox pipefail
 python3 -m venv venv
 source venv/bin/activate
 pip install wheel
